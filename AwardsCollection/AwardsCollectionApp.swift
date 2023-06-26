f12cfc8785e75ab0b2b4b07c9d11977f3ee8fe25
AssanCred <temix@live.ru>
2023-06-26 19:24:24 +0700

    Initial Commit

---
+++ AwardsCollection/AwardsCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionApp.swift
+//  AwardsCollection
+//
+//  Created by Артём Латушкин on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
